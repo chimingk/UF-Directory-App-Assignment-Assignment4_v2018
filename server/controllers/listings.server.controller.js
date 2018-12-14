32cc8433010ee7ef11dd9cfe27eb2d4e07428279
chimingk <chikwan@ufl.edu>
2018-12-13 22:40:20 -0500

    Final submission
    Updated
    server/config/express.js
    server/controllers/listings.server.controller.js
    server/models/listings.server.model.js

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -42,15 +42,40 @@ exports.create = function(req, res) {
 exports.read = function(req, res) {
   /* send back the listing as json from the request */
   res.json(req.listing);
+
+  // What does it look like? console.log(listing);
+
 };
 
 /* Update a listing - Complete the three tasks*/
 exports.update = function(req, res) {
   var listing = req.listing;
 
+  // What does it look like? console.log(listing);
   /* Replace the article's properties with the new properties found in req.body */
   /* save the coordinates (located in req.results if there is an address property) */
+
+  if(req.body.code)
+    listing.code = req.body.code;
+  if(req.body.name)
+    listing.name = req.body.name;
+  if(req.body.address)
+    listing.address = req.body.address;
+  if(req.results){
+    listing.coordinates = {latitude: req.results.lat, longitude: req.results.lng};
+  }
+
   /* Save the article */
+  listing.save(function(err){
+    if(err) {
+      console.log(err);
+      res.status(404).send(err);
+    } else {
+      res.json(listing);
+    }
+  });
+
+
 };
 
 /* Delete a listing */
@@ -58,11 +83,35 @@ exports.delete = function(req, res) {
   var listing = req.listing;
 
   /* Remove the article - Your Code*/
+  listing.remove(function(err){
+    if(err){
+      console.log(err);
+      res.status(404).send(err);
+    } else {
+      res.json(listing);
+    }
+  });
+
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
+/* 
+    https://stackoverflow.com/questions/5825520/in-mongoose-how-do-i-sort-by-date-node-js/15081087#15081087 
+    For sorting descending, it's "-1" but ascending is "1"
+    Room.find({}).sort([['date', -1]]).exec(function(err, docs) { ... });
+
+
+*/
 exports.list = function(req, res) {
-  /* Your code here */
+  Listing.find({}).sort([['code', 1]]).exec(function(err,listings){
+    if(err)
+      res.status(404).send(err);
+    else
+      res.json(listing);
+  });
+
+
+
 };
 
 /* 
--- server/models/listings.server.model.js
+++ server/models/listings.server.model.js
@@ -4,14 +4,55 @@
 var mongoose = require('mongoose'), 
     Schema = mongoose.Schema;
 
-/* Create your schema */
+/* Create your schema for the data in the listings.json file that will define how data is saved in your database
+     See https://mongoosejs.com/docs/guide.html for examples for creating schemas
+     See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+  */
+
+
 var listingSchema = new Schema({
-  /* your code here from Bootcamp Assignment #3 - ListingSchema.js File*/
+
+  /* Some listings have fields missing from one or more of the attributes
+	attribute # 1) code
+	attribute # 2) name
+	attribute # 3) coordinates -> subattributes [latitude, longitude]
+	attribute # 4) address
+ */
+
+
+	code: String,
+	name: String,
+	coordinates: [{latitude: Number, longitude: Number}],
+	address: String
+
 });
 
-/* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
+/* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
+   See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+*/
 listingSchema.pre('save', function(next) {
-  /* your code here from Bootcamp Assignment #3 - ListingSchema.js File */
+
+
+	// Create a Date object and initialize it to today's date
+	var currentDate = new Date();
+
+
+	// Set the listing's update_at date to currentDate
+	this.updated_at = currentDate;
+
+
+	// If the listing's created_at date doesn't exit, then update it to current date
+	if(!this.created_at)
+		this.created_at = currentDate;
+
+	// If the listing's attributes code or name is missing, then fill it in
+	if(this.code == null || this.name == null)
+		return sizeBy.json({success: false, message: 'Listing is incomplete'});
+
+
+	next();
+
+
 });
 
 /* Use your schema to instantiate a Mongoose model */
